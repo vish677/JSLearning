73657e9c43c953aab8330a6e2096b94eacc713d9
vish677 <124337782+vish677@users.noreply.github.com>
2023-02-16 10:41:40 +0530

    01_Variable_assign
    01_Variable_assign

---
+++ 01_variable_assign.js
@@ -0,0 +1,28 @@
+console.log(" ********* Step1.*********");
+
+var myFullName ;
+console.log(myFullName);
+var firstName = "vishal";
+var lastName = "pawar";
+console.log("my full name:",firstName,lastName);
+myFullName = "Vishal";
+myFullName = "Suresh";
+myFullName = "pawar";
+console.log("my full name:",myFullName,myFullName,myFullName);
+
+console.log("************* Step2**********");
+
+var myLove ;
+console.log(" I Love only js" ,myLove);
+
+console.log ("************* Step3***************")
+
+var firstName = "vishal";
+var lastName = "pawar";
+var age = "22";
+var address = "At post Tuljapur";
+var pinCode = "413601";
+var gender = "male";
+var isMaried = "no";
+console.log("first name:",firstName,"last name:",lastName,"age:",age,"address:",address,"pincode:",pinCode,"gender:",gender, "maried status:",isMaried);
+ 
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,23 @@
 console.log(" Vishal Pawar ");
+console.log(31);
+console.log("pune");
+
+
+var fullName = "Vishal Pawar";
+console.log(fullName);
+console.log(fullName);
+
+var city; //  variable Declaration;
+console.log(city);
+city = "pune"; //  variable initialozation
+console.log(city);
+city = "surat"; //  variable update
+
+var age = "31";   //  variable initialozation and initiatization in same line
+age = "32";
+age = "33"
+console.log(age);
+
+var universityName = "NMCE";   //  variable initialozation
+console.log(universityName);
+;
