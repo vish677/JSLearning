81edfcab487c9ddb5f9e9e33595c9b010ac5151d
vish677 <124337782+vish677@users.noreply.github.com>
2023-02-20 18:54:48 +0530

    Create Assignment 03_typeof Datatype
    Create Assignment 03_typeof Datatype

---
+++ 03_typeOf_assign.js
@@ -0,0 +1,16 @@
+console.log("*********step1********");
+var bankName;
+bankName = "SBI Bank";
+console.log("Type of bankname is:",bankName);
+console.log("*********step2********");
+var marks = "90%";
+console.log("type of marks is:", typeof marks);
+console.log("**********Step3**************");
+var isWorking = true;
+console.log("type of isWorking is:",typeof isWorking);
+console.log("**************Step4************");
+var totalCount = "Hundred and Seven";
+console.log("type of totalCount is:",typeof totalCount);
+totalCount = 107;
+console.log("value of totalCount is:",totalCount);
+console.log("type of totalCount is:",typeof totalCount);
