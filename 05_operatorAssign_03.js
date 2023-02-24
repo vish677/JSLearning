61c43104a2db57752f52d2484e1d5cd0fbb37028
vish677 <124337782+vish677@users.noreply.github.com>
2023-02-24 21:32:10 +0530

    Update 05_operatorAssign_03
    Update 05_operatorAssign_03

--- 05_operatorAssign_03.js
+++ 05_operatorAssign_03.js
@@ -1,13 +1,14 @@
 console.log("+++++++++++++++++++++++++++++++++++++++++++++++");
 function tcs(gradScore, hscScore, sscScore, candidateName) {
-      var result = Tcs = "gradScore" >=70 || "hscScore" >=80 || "sscScore" >90 ? `congrates${candidateName} you are eligible for TCS interview`: "you are not eligible better luck next time" ;
-      return result;
+      var result = (gradScore>=70 || hscScore>=80 || sscScore>90) ? `congrates ${candidateName} you are eligible for TCS interview`: "you are not eligible better luck next time" ;
+     console.log(result);
+      
+
 }
- var result = tcs(80,86,90,"Vishal");
- console.log(result);
+tcs(80,86,90,"Vishal");
+ 
 
- var resul = tcs (70, 65, 55,"Anil");
- console.log(resul);
+tcs (70, 65, 55,"Anil");
 
- var results = tcs(4,79,88,"Sunil");
- console.log(results);
+ tcs(4,79,88,"Sunil");
+ 
